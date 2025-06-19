ef0a0ec8175d719fb21cf4d19aa662cc862218a1
DalbanjanRushil <dalbanjanrushil0@gmail.com>
2025-06-19 20:10:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,4 +55,5 @@ def generate_music():
         return jsonify({"error": "HuggingFace call failed", "details": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=False)
+
